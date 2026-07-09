44d35b5667ed50e76fb08552691d097e1d0514d9
MarkovChenITRI <140595764+MarkovChenITRI@users.noreply.github.com>
2026-07-09 17:50:17 +0800

    update

---
+++ docs/assets/site.js
@@ -0,0 +1,8 @@
+﻿const toggle = document.querySelector('[data-mobile-nav-toggle]');
+const sidebar = document.querySelector('.sidebar');
+
+if (toggle && sidebar) {
+  toggle.addEventListener('click', () => {
+    sidebar.classList.toggle('open');
+  });
+}
